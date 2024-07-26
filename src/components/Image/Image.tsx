a8b9532330282ec8b13a5383f8e9d18eff8d4d5e
mirdukkkkk <69540930+mirdukkkkk@users.noreply.github.com>
2024-07-26 13:53:25 +1200

    rewrite to docusaurus

---
+++ src/components/Image/Image.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+type ImageProps = {
+    width: number | string;
+    height?: number | string;
+    name: string;
+    src: string;
+    align: 'left' | 'right' | 'center';
+}
+
+const Image: React.FC = ({ width, height, name, src, align = 'left' }: ImageProps) => (
+    <div style={{ textAlign: align }}>
+        <figure>
+            <img
+                src={src}
+                width={width}
+                height={height}
+                alt={name}
+            />
+            <figcaption><p>{name}</p></figcaption>
+        </figure>
+    </div>
+);
+
+export default Image;
